2df28c5e02416fda8102a5cef166cc5d86815537
Lilythas <charghinofflily@gmail.com>
2026-01-15 23:10:13 +0100

    Initial Unity project

--- Assets/Dark Ghosts FREE/Scripts/anim_clip_offset.cs
+++ Assets/Dark Ghosts FREE/Scripts/anim_clip_offset.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -33,3 +34,40 @@ public class anim_clip_offset : MonoBehaviour
     
 }
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace namespace_animclip_offset{
+public class anim_clip_offset : MonoBehaviour
+{
+    private Animator animator;
+   
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        Play_Animationclip_offset();
+    }
+
+    void Play_Animationclip_offset()
+    {
+            AnimatorClipInfo[] clip_name = animator.GetCurrentAnimatorClipInfo(0);
+            AnimationClip anim_clip = clip_name[0].clip;
+
+            float time = Random.Range(0f, anim_clip.length);
+            int clip_position = animator.GetCurrentAnimatorStateInfo(0).shortNameHash;
+
+            animator.Play(clip_position, 0, time / anim_clip.length);
+            
+
+    }
+    void Update(){
+
+        
+    }
+    
+}
+}
+>>>>>>> 4c1cd61 (initial Unity project setup)
